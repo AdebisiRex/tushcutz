1c72ed4556c0f81b0b4a5103252da212cbf4b061
Adesina Adebisi <104420789+AdebisiRex@users.noreply.github.com>
2024-01-19 11:44:34 +0100

    dep updates 1

--- src/components/GalleryImage.js
+++ src/components/GalleryImage.js
@@ -5,6 +5,7 @@ const GalleryImage = ({img, st_class}) => {
   return (
     <div className={`${classes.image} ${st_class} g-col-6 g-col-md-6  g-col-xl-3`}>
         <img src={img} alt="" />
+        <h1>Hello</h1>
     </div>
   )
 }
